781cca3dca7e72ee3a8c590d122db07d5b96c19e
SiamIslamSagor <msiamislam12345@gmail.com>
2024-07-17 00:21:37 +0600

    setup folder stracture, developed login and registration form with proper design, and access thos input values.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,22 @@
-function App() {
-  return <div>App</div>;
-}
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { lazy } from "react";
+
+const Home = lazy(() => import("./pages/Home"));
+const Login = lazy(() => import("./pages/Login"));
+const Chat = lazy(() => import("./pages/Chat"));
+const Groups = lazy(() => import("./pages/Groups"));
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/chat/:chatId" element={<Chat />} />
+        <Route path="/groups" element={<Groups />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
 export default App;
---
+++ client/src/pages/Login.jsx
@@ -0,0 +1,250 @@
+import {
+  Avatar,
+  Button,
+  Container,
+  IconButton,
+  Paper,
+  Stack,
+  TextField,
+  Typography,
+} from "@mui/material";
+import { useState } from "react";
+import { CameraAlt } from "@mui/icons-material";
+import { VisuallyHiddenInput } from "../components/styles/StyledComponents";
+import { useFileHandler, useInputValidation } from "6pp";
+import { usernameValidator } from "../utils/validators";
+
+function Login() {
+  const [isLogin, setIsLogin] = useState(true);
+
+  const handleToggleLogin = () => setIsLogin(prev => !prev);
+
+  const name = useInputValidation("");
+  const bio = useInputValidation("");
+  const username = useInputValidation("", usernameValidator);
+  const password = useInputValidation("");
+  const avatar = useFileHandler("single");
+
+  const handleSignUp = e => {
+    e.preventDefault();
+  };
+
+  const handleLogin = e => {
+    e.preventDefault();
+  };
+
+  return (
+    <div
+      style={{
+        backgroundImage:
+          "linear-gradient(rgba(125,118,225,0.5),rgba(164,77,77,0.5))",
+      }}
+    >
+      <Container
+        component={"main"}
+        maxWidth="xs"
+        sx={{
+          height: "100vh",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <Paper
+          elevation={3}
+          sx={{
+            padding: 4,
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+          }}
+        >
+          {isLogin ? (
+            <>
+              <Typography variant="h5">Login</Typography>
+              <form
+                style={{
+                  width: "100%",
+                  marginTop: "1rem",
+                }}
+                onSubmit={handleLogin}
+              >
+                <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  variant="outlined"
+                  margin="normal"
+                  value={username.value}
+                  onChange={username.changeHandler}
+                />
+
+                <TextField
+                  required
+                  fullWidth
+                  type="password"
+                  label="Password"
+                  variant="outlined"
+                  margin="normal"
+                  value={password.value}
+                  onChange={password.changeHandler}
+                />
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  type="submit"
+                  sx={{
+                    marginTop: "1rem",
+                  }}
+                >
+                  Login
+                </Button>
+
+                <Typography textAlign={"center"} m={"1rem"}>
+                  OR
+                </Typography>
+              </form>
+              <Button
+                variant="text"
+                color="secondary"
+                type="button"
+                fullWidth
+                onClick={handleToggleLogin}
+              >
+                Sign Up Instead
+              </Button>
+            </>
+          ) : (
+            <>
+              <Typography variant="h5">Sign Up</Typography>
+              <form
+                style={{
+                  width: "100%",
+                  marginTop: "1rem",
+                }}
+                onSubmit={handleSignUp}
+              >
+                <Stack position={"relative"} width="10rem" margin="auto">
+                  <Avatar
+                    sx={{
+                      width: "10rem",
+                      height: "10rem",
+                      objectFit: "contain",
+                    }}
+                    src={avatar.preview}
+                  />
+
+                  <IconButton
+                    sx={{
+                      position: "absolute",
+                      bottom: "0",
+                      right: "0",
+                      color: "white",
+                      bgcolor: "rgba(0,0,0,0.5)",
+                      ":hover": {
+                        bgcolor: "rgba(0,0,0,0.7)",
+                      },
+                    }}
+                    component="label"
+                  >
+                    <>
+                      <CameraAlt />
+                      <VisuallyHiddenInput
+                        type="file"
+                        onChange={avatar.changeHandler}
+                      />
+                    </>
+                  </IconButton>
+                </Stack>
+
+                {avatar.error && (
+                  <Typography
+                    m="1rem auto"
+                    width={"fit-content"}
+                    display={"block"}
+                    color={"error"}
+                    variant="caption"
+                  >
+                    {avatar.error}
+                  </Typography>
+                )}
+
+                <TextField
+                  required
+                  fullWidth
+                  label="Name"
+                  variant="outlined"
+                  margin="normal"
+                  value={name.value}
+                  onChange={name.changeHandler}
+                />
+                <TextField
+                  required
+                  fullWidth
+                  label="Bio"
+                  variant="outlined"
+                  margin="normal"
+                  value={bio.value}
+                  onChange={bio.changeHandler}
+                />
+                <TextField
+                  required
+                  fullWidth
+                  label="Username"
+                  variant="outlined"
+                  margin="normal"
+                  value={username.value}
+                  onChange={username.changeHandler}
+                />
+                {username.error && (
+                  <Typography color={"error"} variant="caption">
+                    {username.error}
+                  </Typography>
+                )}
+                <TextField
+                  required
+                  fullWidth
+                  type="password"
+                  label="Password"
+                  variant="outlined"
+                  margin="normal"
+                  value={password.value}
+                  onChange={password.changeHandler}
+                />
+
+                <Button
+                  variant="contained"
+                  color="primary"
+                  fullWidth
+                  type="submit"
+                  sx={{
+                    marginTop: "1rem",
+                  }}
+                >
+                  Sign Up
+                </Button>
+
+                <Typography textAlign={"center"} m={"1rem"}>
+                  OR
+                </Typography>
+              </form>
+              <Button
+                variant="text"
+                color="secondary"
+                type="button"
+                fullWidth
+                onClick={handleToggleLogin}
+              >
+                Login In Instead
+              </Button>
+            </>
+          )}
+        </Paper>
+      </Container>
+    </div>
+  );
+}
+
+export default Login;
