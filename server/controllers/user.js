60c10bd29d6489a47dd1fa7b230832d85369d2f4
SiamIslamSagor <msiamislam12345@gmail.com>
2024-08-25 19:41:08 +0600

    create custom error handler TryCatch

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,5 +1,7 @@
+import { compare } from "bcrypt";
 import { User } from "../models/user.js";
 import { sendToken } from "../utils/features.js";
+import { TryCatch } from "../middlewares/error.js";
 
 // Create a new user and save it to the database and save in cookie
 const newUser = async (req, res) => {
@@ -23,8 +25,22 @@ const newUser = async (req, res) => {
   sendToken(res, user, 201, "User Created!");
 };
 
-const login = (req, res) => {
-  res.send("Hello Form Login");
+const login = TryCatch(async (req, res, next) => {
+  const { username, password } = req.body;
+
+  const user = await User.findOne({ username }).select("+password"); // .select('+password') means select and give the password filed also, we need to select the password field manually because in User model we write password: { select: false };
+
+  if (!user) return next(new Error("Invalid Username"));
+
+  const isPasswordMatched = await compare(password, user.password);
+
+  if (!isPasswordMatched) return next(new Error("Invalid Password"));
+
+  sendToken(res, user, 200, `Welcome Back, ${user.name}!`);
+});
+
+const getMyProfile = async (req, res) => {
+  // return await User.findById("")
 };
 
-export { newUser, login };
+export { newUser, login, getMyProfile };
